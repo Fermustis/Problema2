2ed5269a53fd8ed423423d8604c3176e3deb133c
Fernando <fgonzalezz1400@hotmail.com>
2023-05-11 14:05:08 -0600

    Add: Primer test

--- Program.cs
+++ Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Fernando Gonzalez Zamora
+
+            // input de datos
+            string input = Console.ReadLine();
+            //convertir a char 
+            char[] inputArray = input.ToCharArray();
+            //Auxiliares
+            int cantidadZ=0;
+            int cantidadO=0;
+            for(int i=0; i<inputArray.Length; i++)
+            {
+                //contando los caracteres
+                if (inputArray[i] == 'z' || inputArray[i] == 'Z')
+                {
+                    cantidadZ++;
+                }
+                else if (inputArray[i] == 'o' || inputArray[i] == 'O' )
+                {
+                    cantidadO++;
+                }
+            }
+            //comparando con la formula 2*x=y
+            if(cantidadZ*2 == cantidadO)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
